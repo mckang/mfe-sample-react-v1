502aef0e9b2e2c134f047d848d658555e1ec2118
thomas kang <mckang@nhndata.com>
2024-08-21 14:34:31 +0900

    header title 수정

--- host-app/src/components/header.jsx
+++ host-app/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <h1 className="project-title">Monolith - TodoApp</h1>
+      <h1 className="project-title">마이크로프론트엔드 - TodoApp React v1</h1>
     </div>
   );
 }
